5bd76dfd332c1d377cc89c8193e7ee20468e7c86
Joe Torreggiani <joetorreggiani@gmail.com>
2022-03-18 23:06:47 -0700

    Update admin labels.

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
+
+admin.site.site_header = 'Insights Agent'
+admin.site.index_title = 'Site Administration'
+admin.site.site_title = 'Insights Agent Administration'
 
 urlpatterns = [
     path('', views.index, name='index'),
